7e9807164e17952590c5fa1e3659f76e1a960034
Dayalan <nairdayalan@gmail.com>
2021-11-08 12:43:32 +0200

    project upload

---
+++ C_programs/hfr.c
@@ -0,0 +1,122 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <unistd.h>
+#include <sys/mman.h>
+#include <fcntl.h>
+#include <string.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+
+// compile with:
+// accepts number of chirps as an argument. Default is 1.
+
+
+
+static int32_t adc1_accumulator [720000];
+static int32_t adc2_accumulator [720000];
+int main(int argc, char **argv)
+{
+	int fd;
+	void *cfg;
+	int16_t *ram;
+	uint8_t *rst;
+	int i, j;
+	FILE *adc1, *adc2;
+	int page = sysconf(_SC_PAGESIZE);
+	int fpga_memory_size = 10*pow(10,6);
+	printf("Allocated fpga memory is %d\n",fpga_memory_size);
+	uint32_t num_pages = fpga_memory_size/page;
+	uint32_t num_bytes = num_pages*page;
+	uint32_t delay = 26; //see formula
+	printf("Size of memory mapped for receiver: %zu\n", num_bytes);
+	const uint32_t num_samples = 720000;
+	printf("Number of samples to be stored: %d\n", num_samples);
+	int num_chirps;
+	//use Julia pinc calculator
+	// uint32_t pinc_min = 5167; 
+	// uint32_t pinc_max = 5570;
+	
+	if (argc == 2)
+		num_chirps = atoi(argv[1]);
+	else num_chirps = 1;
+	
+	
+	if((fd = open("/dev/mem", O_RDWR)) < 0)
+	{
+		perror("open");
+		return 1;
+	}
+	
+	cfg = mmap(NULL, page, PROT_READ|PROT_WRITE, MAP_SHARED, fd, 0x40000000);
+	//configure ram for 90 000 pages (can go up to about 92 285)
+	ram = mmap(NULL, num_bytes, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0x1e000000);
+	
+	printf("cfg address: %p\n", cfg);
+	printf("ram address: %p\n", ram);
+
+	rst = (uint8_t *)(cfg + 0);
+
+	*(uint32_t *)(cfg+4) = num_samples;	// set number of samples to store. address 32-63
+	printf("Number of samples sent to packetiser\n");
+		
+	*(uint32_t *)(cfg+8) = delay;//set delay -> stretches the chirp
+	printf("Pinc delay set to %d (determines chirp duration)\n",delay);
+	
+	// *(uint32_t *)(cfg+12) = pinc_min;
+	// printf("Pinc min = %d\n",pinc_min);
+	
+	// *(uint32_t *)(cfg+16) = pinc_max;
+	// printf("Pinc max = %d\n",pinc_max);
+
+	// intialise pinc_FSM
+	// populates pinc array and sets delay using assigned values (see above)
+	*rst &= ~8;
+	*rst |= 8;
+	printf("initialised phase incrementor FSM.\n");
+	//clear init pin (good practice) and ensure gen set to 0
+	*rst &= ~8;
+	*rst &= ~4;
+
+	printf("Number of chirps to be accumulated: %d\n", num_chirps);
+	
+	//repeat experiment for accumulating data. improves SNR
+	for (j = 0;j<num_chirps;j++){
+		printf("Iteration %d\n",j);
+		//DDS requires rst for min 2 clck cycles
+		*rst &= ~16;
+		usleep(1);//no need for precise sleep
+		*rst |= 16;
+		usleep(100); //give DDS more time to warm up
+		printf("DDS reset \n");
+		*rst &= ~7; //rst writer = rst packetiser = pinc_gen = 0;
+		*rst |= 7;
+		*rst &= ~4;//turn off gen for chirp
+		printf("Reset writer, reset packetiser and started chirp\n");
+		printf("Accumulating...\n");
+		for(i = 0; i < num_samples; ++i)
+		{
+			adc1_accumulator[i] += ram[2 * i];
+			adc2_accumulator[i] += ram[2 * i + 1];
+		}
+	
+	}
+	
+	
+	adc1 = fopen("adc1.txt","w+");
+	adc2 = fopen("adc2.txt","w+");
+	printf("Accumulation complete. Storing data\n");
+	for(i = 0; i < num_samples; ++i)
+	{
+		fprintf(adc1, "%d\n",adc1_accumulator[i]);
+		fprintf(adc2, "%d\n",adc2_accumulator[i]);
+	}
+		
+		
+	printf("Data recorded. closing files...\n");
+	fclose(adc1);
+	fclose(adc2);
+	munmap(cfg, page);
+	munmap(ram, num_bytes);
+	return 0;
+}
---
+++ Verilog/sonar/pinc_FSM.v
@@ -0,0 +1,79 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: UCT
+// Engineer: Dayalan Nair
+// 
+// Create Date: 21.09.2021 09:59:36
+// Design Name: 
+// Module Name: phase_inc_v3
+// Project Name: 
+// Target Devices: Any
+// Tool Versions: 
+// Description: 
+// Provides incremented phase to a DDS Compiler to generate a chirp function for use in HF Radar
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments: Generates chirp only. Optimised for speed. Further optimisation by incl. LUT is possible
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module pinc_FSM(
+    input clk,
+    input init,
+    input gen,
+    input[31:0] delay,
+    output [23:0] pinc,
+    output m_axis_tvalid
+);
+    reg [23:0] pinc_array [500:0]; //set this based on pinc_max - pinc min
+    parameter[0:0] OFF = 0;
+    parameter[0:0] ON = 1;
+    reg state = OFF;
+    integer i;
+    integer j;
+    integer k;
+    reg valid;
+    //values pre-calculated to improve performance
+    parameter [23:0] pinc_min= 5167;
+    parameter [23:0] pinc_max= 5570;
+    parameter [23:0] num_increments = pinc_max-pinc_min;
+    reg [23:0] pinc_out;
+    //loop for 7000 should give 14us for each pinc
+    reg[31:0] dly = 1;
+    assign  pinc = pinc_out;
+    assign m_axis_tvalid = valid;
+    always@(posedge init) begin
+        dly = delay;
+        for (i = 0 ; i <= num_increments ; i = i+1)
+            begin
+                pinc_array[i] = pinc_min + i;
+            end
+    end
+    always@ (posedge clk) begin
+        case(state)
+            OFF: begin
+                if (gen) state = ON;
+                pinc_out = 0;
+                valid = 0;
+                j  = 0;
+                k = 0;
+            end
+            ON: begin
+                valid = 1;
+                if (k<delay) begin
+                    k = k + 1;
+                    pinc_out  = pinc_array[j];
+                end
+                else if (j >= num_increments) state = OFF;
+                    
+                else begin
+                    j = j + 1;
+                    k = 0;
+                end
+            end
+        endcase
+    end
+endmodule
